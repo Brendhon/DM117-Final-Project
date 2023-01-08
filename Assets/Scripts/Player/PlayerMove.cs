bfdfa4f5c25ff3300192cb584b6aa907f20ebbeb
Brendhon <brendhon.e.c.m@gmail.com>
2023-01-08 18:26:56 -0300

    Added horizontal movement

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,20 +6,47 @@ public class PlayerMove : MonoBehaviour
 {
     // Speed flag
     public float speed = 3;
+    public float horizontalSpeed = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check if game object not reached the limit
+        var reachedLeftLimit = this.gameObject.transform.position.x > LevelBoundary.leftSideLimit; 
+        var reachedRigthLimit = this.gameObject.transform.position.x < LevelBoundary.rightSideLimit; 
+
         // Form speed vector
         var speedVector = speed * Time.deltaTime * Vector3.forward;
 
         // Move player
         transform.Translate(speedVector, Space.World);
+
+        // If user press A or Left Arrow AND if game object not reached the limit - Moves to left
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && reachedLeftLimit)
+        {
+
+            // Form speed vector
+            speedVector = horizontalSpeed * Time.deltaTime * Vector3.left;
+
+            // Move player
+            transform.Translate(speedVector, Space.World);
+        }
+
+
+        // If user press D or Right Arrow AND if game object not reached the limit - Moves to rigth 
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && reachedRigthLimit)
+        {
+            // Form speed vector
+            speedVector = horizontalSpeed * Time.deltaTime * Vector3.right;
+
+            // Move player
+            transform.Translate(speedVector, Space.World);
+        }
     }
 }
